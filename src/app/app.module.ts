f0dff90e2432d944dd48ce9381bde3bd1a1d00f5
Splendid Infosystems <splendidinfosystems@Mac.local>
2023-08-19 18:09:16 +0530

    category added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { NavHeaderComponent } from './Components/Header/nav-header/nav-header.component';
 import { BodyParentModuleComponent } from './Components/body/body-parent-module/body-parent-module.component';
 import { FooterComponent } from './Components/Footer/footer/footer.component';
+import { AdvertisementComponent } from './Components/Body/advertisement/advertisement/advertisement.component';
+import { CategoryListComponent } from './Components/Body/category/category-list/category-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavHeaderComponent,
     FooterComponent,
-    BodyParentModuleComponent
+    BodyParentModuleComponent,
+    AdvertisementComponent,
+    CategoryListComponent
   ],
   imports: [
     BrowserModule,
