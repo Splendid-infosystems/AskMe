e5db9210065835ce2fbcf9b7ee7ccc9ee45e8058
Virendra Gayakwad <62784876+Viren-hub@users.noreply.github.com>
2023-08-19 20:32:53 +0530

    Added Search bar

--- src/app/Components/Body/body-parent-module/body-parent-module.component.ts
+++ src/app/Components/Body/body-parent-module/body-parent-module.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./body-parent-module.component.css']
 })
 export class BodyParentModuleComponent {
+  names: string[] = ['Restaurants', 'Hotels', 'Beauty Spa', 'Home Decor']; // Add more names as needed
+  currentNameIndex = 0;
+  currentName = this.names[this.currentNameIndex];
 
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.startNameChangeInterval();
+  }
+
+  startNameChangeInterval() {
+    setInterval(() => {
+      this.changeName();
+    }, 10000); // Change name every 10 seconds
+  }
+
+  changeName() {
+    this.currentNameIndex = (this.currentNameIndex + 1) % this.names.length;
+    this.currentName = this.names[this.currentNameIndex];
+  }
 }
