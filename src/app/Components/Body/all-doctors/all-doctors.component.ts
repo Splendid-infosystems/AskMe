e37a171821c08c98ee2b135f4b0d5a8bc7bead0e
Virendra Gayakwad <62784876+Viren-hub@users.noreply.github.com>
2023-08-27 16:31:47 +0530

    Added See All button

--- src/app/Components/Body/all-doctors/all-doctors.component.ts
+++ src/app/Components/Body/all-doctors/all-doctors.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./all-doctors.component.css']
 })
 export class AllDoctorsComponent {
+  categories = [
+    {
+      label: 'A',
+      showAll: false,
+      items: [
+        { icon: 'fas fa-allergies', name: 'Allergist' },
+        { icon: 'fa-solid fa-mask-face', name: 'Anesthesiologist' },
+        { icon: 'fa fa-deaf', name: 'Audiologist' },
+      ]
+    },
+    {
+      label: 'B',
+      showAll: false,
+      items: [
+        { icon: 'fa-solid fa-syringe', name: 'Bariatric' },
+      ]
+    },
+  ];
 
+  toggleItemsVisibility(category: any) {
+    category.showAll = !category.showAll;
+  }
 }
