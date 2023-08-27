ad0f82a05044a7a6c6a5a55f52a2226a1d9a61f6
Splendid Infosystems <splendidinfosystems@Mac.local>
2023-08-27 17:11:20 +0530

    images added in advertisement

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AllDoctorsComponent } from './Components/Body/all-doctors/all-doctors.component';
 import { BodyParentModuleComponent } from './Components/body/body-parent-module/body-parent-module.component';
-
+import { DoctorDescriptionComponent } from './Components/Body/doctor-description/doctor-description.component';
 const routes: Routes = [
   {
-    path:'AllDoctors', component: AllDoctorsComponent
+    path:'AllDoctors', component: AllDoctorsComponent, children:[
+      {
+        path:'doctordescription', component: DoctorDescriptionComponent
+      }
+    ]
   },
   {
     path:'home', component: BodyParentModuleComponent
