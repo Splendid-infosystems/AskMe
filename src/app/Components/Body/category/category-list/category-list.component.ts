dca462c0025cbc124ae25510d1818346139872a9
Virendra Gayakwad <62784876+Viren-hub@users.noreply.github.com>
2023-08-27 14:07:13 +0530

    Added See All button

--- src/app/Components/Body/category/category-list/category-list.component.ts
+++ src/app/Components/Body/category/category-list/category-list.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./category-list.component.css']
 })
 export class CategoryListComponent {
+  categories = [
+    {
+      label: 'Doctors',
+      showAll: false,
+      items: [
+        { icon: 'fas fa-brain', name: 'Neurologist' },
+        { icon: 'fa-solid fa-tooth', name: 'Dentist' },
+        { icon: 'fas fa-brain', name: 'Neurologist' },
+        { icon: 'fa-solid fa-tooth', name: 'Dentist' },
+        { icon: 'fas fa-brain', name: 'Neurologist' },
+        { icon: 'fa-solid fa-tooth', name: 'Dentist' },
+      ]
+    },
+  ];
 
+  toggleItemsVisibility(category: any) {
+    category.showAll = !category.showAll;
+  }
 }
+
+
